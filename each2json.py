965820317aba507741235a6870840236137ba3c3
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-01-14 13:34:17 -0500

    some documenting `pptx2json.py`, `each2json.py`

--- each2json.py
+++ each2json.py
@@ -4,12 +4,15 @@ r'''
  Converts a PowerPoint presentation to JSON.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2022-01-14t12:53
- Version   : 1.0.0
+ When      : 2022-01-14t13:15
+ Version   : 1.0.1
 
  CHANGELOG :
+    v1.0.1 - 2022-01-14t13:15
+        added documentation
+
     v1.0.0 - 2022-01-14t12:53
-        abstracted from pptx2json.py
+        abstracted from `pptx2json.py`
         opening each file
 
     v0.1.0 - 2021-12-31t02:53
@@ -22,6 +25,14 @@ r'''
  '''
 
 def jsoneach(filenames, openfunc):
+    r'''
+     Prints json representations of objects representing each file
+     specified by the filenames created using the openfunc.
+     @param filenames : [str] = list of names of files
+     @param openfunc : function = to create the object representing an
+        open file
+     @minver 1.0.0
+     '''
     # loop through arguments
     for filename in filenames:
         print(openfunc(filename))
--- pptx2json.py
+++ pptx2json.py
@@ -4,10 +4,13 @@ r'''
  Converts a PowerPoint presentation to JSON.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2021-12-31t02:53
- Version   : 1.0.0
+ When      : 2022-01-14t13:17
+ Version   : 1.0.1
 
  CHANGELOG :
+    v1.0.1 - 2022-01-14t13:17
+        added documentation
+
     v1.0.0 - 2022-01-14t12:59
         abstracted `each2json.py`
         opening each file
@@ -24,6 +27,12 @@ from each2json import jsoneach
 from pptx import Presentation
 
 def main(argv):
+    r'''
+     Prints json representations of Presentations specified by the
+     filenames in arguments argv.
+     @param argv : [str] = commandline arguments containing filenames
+     @minver 0.1.0
+     '''
     jsoneach(argv[1:], Presentation)
 # end def main(argv)
 
