90482900c84794b309fa7412ca4f043668468445
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-01-14 14:45:05 -0500

    `asdict` handles arrays

--- dictclasses.py
+++ dictclasses.py
@@ -4,8 +4,8 @@ r'''
  classes.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2022-01-14t14:36
- Version   : 1.2.1
+ When      : 2022-01-14t14:43
+ Version   : 1.3.1
  '''
 
 # primitive types in valid JSON
@@ -17,14 +17,21 @@ JSON_OBJECTS = ( dict, )
 
 def asdict(obj):
     r'''
-     Converts an object to a dictionary
+     Converts an object to a dictionary.
      @param obj : object = to convert
      @minver 0.0.0
      @return a dictionary representation of the given object
      '''
     # if a primitive, return it
-    #if (isinstance(obj, JSON_PRIMITIVES)):
-    #    return obj
+    if (isinstance(obj, JSON_PRIMITIVES)):
+        return obj
+    # end if (isinstance(obj, JSON_PRIMITIVES))
+
+    # if an array, convert each member
+    if (isinstance(obj, JSON_ARRAYS)):
+        return [ asdict(elem) for elem in obj ]
+    # end if (isinstance(obj, JSON_ARRAYS))
+
     # initialize to the dictionary of the object
     result = obj.__dict__
     # replace each object with a 
