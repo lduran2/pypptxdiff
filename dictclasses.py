c436e4f41ca80cb58fa1c2ca7a099c0b386fd915
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-01-16 00:51:16 -0500

    using `dir` to iterate objects

--- dictclasses.py
+++ dictclasses.py
@@ -4,11 +4,12 @@ r'''
  classes.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2022-01-14t20:17
- Version   : 1.5.2-beta
+ When      : 2022-01-15t22:57
+ Version   : 1.5.3-alpha
  '''
 
 from collections import OrderedDict # to preserve order
+from inspect import ismethod
 
 # primitive types in valid JSON
 JSON_PRIMITIVES = ( str, int, bytes, float, bool )
@@ -56,23 +57,40 @@ def asdict(obj, visited=[]):
         # convert each value
         # stringify each key
         converted = { str(key): asdict(value, visited)
-                      for (key, value) in obj.items() }
+                      for (key, value) in obj.items()  }
         # preserves order of insertion
         ordered = OrderedDict(converted)
         # return the result
         return ordered
     # end if (isinstance(obj, JSON_OBJECTS))
 
+    # otherwise, if a Python object:
+    
     # if a Python object:
     try:
-        # get the dictionary of the object
-        obj_dict = obj.__dict__
-    # end try obj.__dict__
-    # if type error, then obj does not support __dict__:
+        # get the names of the attributes of the object
+        obj_attr_names = dir(obj)
+    # if type error, then obj does not support __dict__,
+    # thus dir either:
     except TypeError:
         # so return the object itself
         return None
+    # get the public fields of the object
+    obj_fields = { name: attr
+                   for (name, attr) in
+                   ( (name, getattr(obj, name))
+                     for name in obj_attr_names
+                     if (
+                        # double checking attributes
+                        hasattr(obj, name)
+                        # ignoring private attributes
+                        and not(name.startswith(r'_'))
+                     )
+                   )
+                   # and ignoring function
+                   if (not(callable(attr)))
+                 }
     # recursively process the dictionary
-    return asdict(obj_dict, visited)
+    return asdict(obj_fields, visited)
     # end except TypeError
 # end def asdict(obj)
